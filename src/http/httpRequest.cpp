7b7b4e8d05ce6f45a6f1dde886a8a532cc19f1c9
Linna <haolinna23@gmail.com>
2026-06-01 12:31:44 +0200

    request line parse finish

--- src/http/httpRequest.cpp
+++ src/http/httpRequest.cpp
@@ -1,23 +1,48 @@
-#include "httprequest.hpp"
+#include"httpRequest.hpp"
 
-void httpRequest::setMethod(const std::string &m);
-void httpRequest::setPath(onst std::string &p);
-void httpRequest::setVersion(onst std::string &v);
-void httpRequest::setHeader(const std::string &key, const std::string &value);
-
-
-void parseRequestLine(cosnt std::string &buffer, httpRequest req)
+static std::vector<std::string> splitBySpace(const std::string &requestLine)
 {
-    // TBD
+    std::vector<std::string> results;
+    std::string part;
+
+    std::istringstream iss(requestLine);
+    while(iss >> part)
+        results.push_back(part);
+    return results;
 }
 
-httpRequest parseRequest(const std::string &request_buffer)
+bool httpRequest::parseRequestLine(const std::string &requestLine) // need to check line
 {
-    httpRequest req;
+    std::vector<std::string> parts;
 
-    parseRequestLine(request_buffer, req);
-    parseHeaders(request_buffer, req);
+    parts = splitBySpace(requestLine);
+    if (parts.size() != 3)
+        return false;
+    method = parts[0];
+    path = parts[1];
+    version = parts[2];
+    return true;
+}
+
+static std::string getRequestLine(const std::string& request)
+{
+    size_t pos = request.find("\r\n");
+    if (pos == std::string::npos)
+        return "";
+    return (request.substr(0, pos));
+}
+
+bool httpRequest::parseRequest(const std::string &request)
+{
+    if (request.empty())
+        return false;
+    std::string requestLine = getRequestLine(request);
+    if (requestLine.empty())
+        return false;
+    if (!parseRequestLine(requestLine))
+        return false;
+    // parseHeaders(request_buffer, req);
     // parseBody(buffer, req); // 第一周先空着
-    return req;
+    return true;
 }
 
--- src/http/httpRequest.hpp
+++ src/http/httpRequest.hpp
@@ -4,6 +4,7 @@
 # include <iostream>
 # include <string>
 # include <map>
+# include <vector>
 
 class httpRequest
 {
@@ -14,12 +15,8 @@ class httpRequest
         std::map<std::string, std::string> headers;
         std::string body;
     public:
-        void setMethod(const std::string &m);
-        void setPath(onst std::string &p);
-        void setVersion(onst std::string &v);
-        void setHeader(const std::string &key, const std::string &value);
-}
-
-httpRequest parseRequest(const std::string &request_buffer);
+        bool parseRequest(const std::string &request);
+        bool parseRequestLine(const std::string &requestLine);
+};
 
 #endif
